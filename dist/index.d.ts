dcdab867a4f974185bd00bafc2f0399839320991
Dmitri Tanasiitsuk <dmitri.tanasiitsuk@bigwintek.com>
2024-08-14 12:29:28 +0300

    dist folder should be deployed

---
+++ dist/index.d.ts
@@ -0,0 +1,8 @@
+import { FunctionComponentElement } from 'react';
+export { Button, buttonVariants } from '@/components/ui/button';
+
+interface Props {
+}
+declare function export_default(props: Props): FunctionComponentElement<Props>;
+
+export { export_default as TestComponent };
